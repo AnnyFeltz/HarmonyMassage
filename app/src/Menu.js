d64d7ecea53da0b53c6ddff4e8e0de0237582d49
Kayure <luccas_kayure@hotmail.com>
2025-02-05 11:19:46 -0300

    commit: configurando

---
+++ app/src/Menu.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import { Link } from 'react-router-dom';
+
+function Menu() {
+    return (
+        <div>
+            
+        </div>
+    );
+} export default Menu;
---
+++ app/src/Pagina5.js
@@ -0,0 +1,9 @@
+import react from 'react';
+
+function Pagina1() {
+    return (
+        <div>
+            
+        </div>
+    );
+} export default Pagina1;
