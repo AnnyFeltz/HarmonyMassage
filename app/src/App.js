6b0b4ad9ecb4e820961b72f110e4c3618a152532
luanamoura02 <luanab.moura125@gmail.com>
2025-02-08 11:10:52 -0300

    n fiz nada, vo esperar alguem fz o home pra ter uma ideia de cm fz o resto

--- app/src/App.js
+++ app/src/App.js
@@ -14,7 +14,7 @@ function App() {
             avaliar
             - cliente
             - massagista
-            - gerente
+            - gerentee
 
             editar massagistas
             - gerente
